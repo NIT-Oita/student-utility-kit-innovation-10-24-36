5293d1b1e7b5d5c5c1a5ffeb0e3b55907aad5791
s25240209 <s2524@oita.kosen-ac.jp>
2026-06-22 18:01:35 +0900

    Update storage.h

--- storage.h
+++ storage.h
@@ -1,9 +1,6 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
-void newword(char word[1000],char mean[1000]);
-void card(void);
-void midasi(char str[]);
-void kaigyo(int kaisu);
+void newword(char word[1000], char mean[1000]);
 
 #endif
