81c7c22c2c9ea26ad4af4860354a348a97e302e6
s25240209 <s2524@oita.kosen-ac.jp>
2026-06-22 18:39:55 +0900

    Update storage.c

--- storage.c
+++ storage.c
@@ -1,14 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void newword(char word[1000], char mean[1000]){
+// 単語と意味をwords.txtに保存する関数
+void newword(char word[256], char mean[256]){
+    // ファイルを開く
     FILE *fp = fopen("words.txt", "a");
 
-    if( fp == NULL ){
+    // ファイルを開けなかった場合
+    if(fp == NULL){
         printf("ファイルを開けませんでした");
         return;
     }
 
+    // 単語と意味をファイルへ書き込む
     fprintf(fp, "%s %s\n", word, mean);
     printf("word: %s, mean: %sで単語帳に登録しました！\n", word, mean);
+
+    // ファイルを閉じる
     fclose(fp);
 }
